75f7d846922d60a323287b431fc30717eccd390d
Nora Galvin <minhnora98@gmail.com>
2019-06-01 15:54:05 +0700

    Init Database

---
+++ database/migrations/2019_06_01_072837_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer("category_id")->nullable()->index();
+            $table->integer("user_id")->nullable()->index();
+            $table->string("title");
+            $table->string("content");
+            $table->string("image_url");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
